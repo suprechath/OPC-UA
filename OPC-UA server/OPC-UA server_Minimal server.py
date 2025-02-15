087b09b5ba863e2dff9fc63ba5b9879f7db0e88f
suprechath <suprechathongsakulpanich@gmail.com>
2025-02-15 23:51:31 +0700

    Starting

---
+++ OPC-UA server/OPC-UA server_Minimal server.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.insert(0, "..")
+import time
+
+from opcua import ua, Server
+
+if __name__ == "__main__":
+
+    # setup our server
+    server = Server()
+    # server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+    server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = server.register_namespace(uri)
+
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = objects.add_object(idx, "MyObject")
+    myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+    myvar.set_writable()    # Set MyVariable to be writable by clients
+
+    # starting!
+    server.start()
+    print("OPC-UA Server started at ", uri)
+    
+    try:
+        count = 0
+        while True:
+            time.sleep(1)
+            count += 0.1
+            myvar.set_value(count)
+    # finally:
+    except KeyboardInterrupt:
+        #close connection, remove subcsriptions, etc
+        server.stop()
+        print("\nShutting down server...")
