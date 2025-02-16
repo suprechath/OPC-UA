c60b9a0385593051c1715b48f0d37891b1baa60c
suprechath <suprechathongsakulpanich@gmail.com>
2025-02-16 09:42:38 +0700

    Simple version_fimished

--- OPC-UA Client/Client_simpleGetValue.py
+++ OPC-UA Client/Client_simpleGetValue.py
@@ -1,56 +1,43 @@
 import sys
-# sys.path.insert(0, "..")
+sys.path.insert(0, "..")
 from opcua import Client, ua
+import time
 
 
 if __name__ == "__main__":
 
     # connect to the created server -> server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
     client = Client("opc.tcp://localhost:4840/freeopcua/server/")
-    
+
+    client.connect()
+    print("Connected to OPC UA server")
+
+    # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+    root = client.get_root_node()
+    print("Server node#: ", root)
+
+    # Node objects have methods to read and write node attributes as well as browse or populate address space
+    print("Children of root are: ", root.get_children())
+
+    m1_temp = root.get_child(["0:Objects","2:Machine1","2:Temperature"])
+    m1_pressure = root.get_child(["0:Objects","2:Machine1","2:Pressure"])
+    m1_speed = root.get_child(["0:Objects","2:Machine1","2:Speed"])
+    m2_temp = root.get_child(["0:Objects","2:Machine2","2:Temperature"])
+    m2_pressure = root.get_child(["0:Objects","2:Machine2","2:Pressure"])
+    m2_speed = root.get_child(["0:Objects","2:Machine2","2:Speed"])
 
     try:
-        client.connect()
-        print("Connected to OPC UA server")
-
-        # uri = "http://examples.freeopcua.github.io"
-        # idx = client.get_namespace_index(uri)
-        # print(idx)
-
-        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
-        root = client.get_root_node()
-        print("Server node#: ", root)
-
-        # Node objects have methods to read and write node attributes as well as browse or populate address space
-        print("Children of root are: ", root.get_children())
-
-        # Now getting a variable node using its browse path
-        # m1_temp = client.get_node(f"ns={idx};s=Machine1.Temperature")
-        # m1_pressure = client.get_node(f"ns={idx};s=Machine1.Pressure")
-        # m1_speed = client.get_node(f"ns={idx};s=Machine1.Speed")
-        m1_temp = root.get_child(["0:Objects","2:Machine1","2:Temperature"])
-        m1_pressure = root.get_child(["0:Objects","2:Machine1","2:Pressure"])
-        m1_speed = root.get_child(["0:Objects","2:Machine1","2:Speed"])
-
-        # myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
-        # obj = root.get_child(["0:Objects", "2:MyObject"])
-        # print("myvar is: ", myvar)
-        # print("myobj is: ", obj)
-
-        # Read value
-        m1_temp_val = m1_temp.get_value()
-        m1_pressure_val = m1_pressure.get_value()
-        m1_speed_val = m1_speed.get_value()
-        print(f"{m1_temp_val},{m1_pressure_val},{m1_speed_val}")
-
-        # # Write a new value
-        # myvar.set_value("Hello OPC UA")
-        # print("New value written")
-
-        # # Read again
-        # updated_value = myvar.get_value()
-        # print(f"Updated Value: {updated_value}")
-
+        while True:
+            # Read value
+            m1_temp_val = m1_temp.get_value()
+            m1_pressure_val = m1_pressure.get_value()
+            m1_speed_val = m1_speed.get_value()
+            print(f"Machine1 -> \n  Temp: {m1_temp_val} °C\n  Pressure: {m1_pressure_val} kPa\n  Speed: {m1_speed_val} RPM")
+            m2_temp_val = m2_temp.get_value()
+            m2_pressure_val = m2_pressure.get_value()
+            m2_speed_val = m2_speed.get_value()
+            print(f"Machine2 -> \n  Temp: {m2_temp_val} °C\n  Pressure: {m2_pressure_val} kPa\n  Speed: {m2_speed_val} RPM")
+            time.sleep(10)
     except KeyboardInterrupt:
         client.disconnect()
         print("\nClient disconnected.")
